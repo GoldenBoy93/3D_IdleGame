981e4f52e3f26f21042cd2feab215d5714311450
minjeparkk <zone8572@gmail.com>
2025-08-27 22:48:07 +0900

    Remove
    필요없는 외부에셋 삭제 및 주석삭제

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -57,7 +57,6 @@ public class GameManager : MonoBehaviour
         if (OnStartWave != null)
         {
             OnStartWave(enemyCount);
-            Debug.Log($"Next : {enemyCount}");
         }
     }
 
